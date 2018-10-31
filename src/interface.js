0d28ffff3aa8723bee3e2543233d230744f41b2b
Reka <rekapap28@gmail.com>
2018-10-31 22:18:08 +0000

    API

--- src/interface.js
+++ src/interface.js
@@ -36,4 +36,22 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=53d3f7239274b9a705f8ba4e7569eda7';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  };
+
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+  
 });
